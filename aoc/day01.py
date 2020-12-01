ec08e473fb8f8f0915e4252e68a70e1dcd17f020
Stephen Mizell <smizell@gmail.com>
2020-12-01 10:38:54 -0600

    Fix types

--- aoc/day01.py
+++ aoc/day01.py
@@ -8,21 +8,23 @@ from typing import List, Tuple
 nums = load_number_file(Path("files/day01.txt"))
 
 
-def find_match(nums: List[int], looking_for: int, matches: int) -> Tuple[int, int]:
+def find_match(nums: List[int], looking_for: int, matches: int) -> List[int]:
     """
     >>> nums = [1721, 979, 366, 299, 675, 1456]
     >>> find_match(nums, looking_for=2020, matches=2)
-    (1721, 299)
+    [1721, 299]
     >>> find_match(nums, looking_for=2020, matches=3)
-    (979, 366, 675)
+    [979, 366, 675]
+    >>> find_match([1,2], looking_for=1000, matches=2)
+    []
     """
     search_nums = (nums[l:] for l in range(matches))
     results = (
-        values
+        list(values)
         for values in itertools.product(*search_nums)
         if sum(values) == looking_for
     )
-    return next(results, None)
+    return next(results, [])
 
 
 if __name__ == "__main__":
