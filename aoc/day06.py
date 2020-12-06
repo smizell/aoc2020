97bf23a1a00859eddba83909ffe4616c4593ee3c
Stephen Mizell <smizell@gmail.com>
2020-12-06 00:22:59 -0600

    Better use of sets

--- aoc/day06.py
+++ aoc/day06.py
@@ -5,5 +5,5 @@ groups = [line.split("\n") for line in inputs.split("\n\n")]
 
 
 if __name__ == "__main__":
-    print(sum([len(set("".join(group))) for group in groups]))
-    print(sum([len(set(g[0]).intersection(*map(set, g[1:]))) for g in groups]))
+    print(sum([len(set.union(*map(set, g))) for g in groups]))
+    print(sum([len(set.intersection(*map(set, g))) for g in groups]))
