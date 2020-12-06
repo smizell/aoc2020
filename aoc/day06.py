12c8b2130222db7a4155d64f090a9f42dea43a60
Stephen Mizell <smizell@gmail.com>
2020-12-05 23:50:35 -0600

    Trying out one liners take 3

--- aoc/day06.py
+++ aoc/day06.py
@@ -4,14 +4,6 @@ inputs = Path("files/day06.txt").read_text().strip()
 groups = [line.split("\n") for line in inputs.split("\n\n")]
 
 
-def part1():
-    print(sum([len(set("".join(group))) for group in groups]))
-
-
-def part2():
-    print(sum([len(set(g[0]).intersection(*map(set, g[1:]))) for g in groups]))
-
-
 if __name__ == "__main__":
-    part1()  # 6506
-    part2()  # 3243
+    print(sum([len(set("".join(group))) for group in groups]))
+    print(sum([len(set(g[0]).intersection(*map(set, g[1:]))) for g in groups]))
