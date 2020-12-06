ec4b2f82ea759c6698fdf5ffb35e3edbb1237a5a
Stephen Mizell <smizell@gmail.com>
2020-12-06 00:31:21 -0600

    More lines, but more readable

--- aoc/day06.py
+++ aoc/day06.py
@@ -2,8 +2,9 @@ from pathlib import Path
 
 inputs = Path("files/day06.txt").read_text().strip()
 groups = [line.split("\n") for line in inputs.split("\n\n")]
+group_sets = [list(map(set, g)) for g in groups]
 
 
 if __name__ == "__main__":
-    print(sum([len(set.union(*map(set, g))) for g in groups]))
-    print(sum([len(set.intersection(*map(set, g))) for g in groups]))
+    print(sum([len(set.union(*g)) for g in group_sets]))
+    print(sum([len(set.intersection(*g)) for g in group_sets]))
