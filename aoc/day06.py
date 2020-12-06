17ee88e6ad7a9d8e0a7dacfb25e6787bf06bcb47
Stephen Mizell <smizell@gmail.com>
2020-12-06 00:38:38 -0600

    Simplify. Last change.

--- aoc/day06.py
+++ aoc/day06.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 
-inputs = Path("files/day06.txt").read_text().strip()
-groups = [line.split("\n") for line in inputs.split("\n\n")]
-group_sets = [list(map(set, g)) for g in groups]
+groups = [
+    [set(g) for g in line.split("\n")]
+    for line in Path("files/day06.txt").read_text().strip().split("\n\n")
+]
 
 
 if __name__ == "__main__":
-    print(sum([len(set.union(*g)) for g in group_sets]))
-    print(sum([len(set.intersection(*g)) for g in group_sets]))
+    print(sum([len(set.union(*ps)) for ps in groups]))
+    print(sum([len(set.intersection(*ps)) for ps in groups]))
