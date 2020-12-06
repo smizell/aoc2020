9af9c50a468cd2f68b146d57291a2fcd4fb3b9ef
Stephen Mizell <smizell@gmail.com>
2020-12-05 23:48:35 -0600

    Trying out one liners take 2

--- aoc/day06.py
+++ aoc/day06.py
@@ -5,16 +5,11 @@ groups = [line.split("\n") for line in inputs.split("\n\n")]
 
 
 def part1():
-    print(calc_sum([set("".join(group)) for group in groups]))
+    print(sum([len(set("".join(group))) for group in groups]))
 
 
 def part2():
-    print(calc_sum([set(g[0]).intersection(*map(set, g[1:])) for g in groups]))
-
-
-def calc_sum(answer_sets):
-    answers = [len(answer_set) for answer_set in answer_sets]
-    return sum(answers)
+    print(sum([len(set(g[0]).intersection(*map(set, g[1:]))) for g in groups]))
 
 
 if __name__ == "__main__":
